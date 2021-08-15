09a157e89198f90502dacd8025bb704958ef93c4
Kris Hatch-McCarter <userkris169@yahoo.com>
2021-08-15 14:28:48 -0700

    Readme and Pseduocode

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# treehouse-FSJSTechDegree-Project05
+My Treehouse FSJS TechDegree Project 5 - Public API Requests
 
-In this project, I built an employee directory for Awesome Startup, a fictional company, using Random User Generator API to grab information for 12 random "employees" and display it in a grid. When an image or name is clicked, a modal window will open with more details about the employee.
+In this project, I built an employee directory for Awesome Startup, a fictional company. This project uses Javascript, Fetch API and a public API (https://randomuser.me/). Fetch is used to request results for 12 random "employees" and Javascript is used to dynamically update the HTML to display those results in a grid. When an image or name inthe grid is clicked, a modal window will open with more details about the employee.
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
-//fetch 12 random users
+//DOM SELECTION
+const gallery = document.getElementById("gallery");
+
+//SINGLE REQUEST TO GET AND DISPLAY 12 RANDOM USERS
 
 fetch("https://randomuser.me/api/?results=12")
   .then((response) => response.json())
   .then((data) => console.log(data.results));
+
+//INCLUDE IMAGE, FIRST NAME AND LAST NAME, EMAIL, CITY OR LOCATION (REFER TO MOCKUPS AND COMMENTS IN INDEX.HTML FOR WHAT SHOULD BE DISPLAYED AND HOW STYLED)
+
+//create div tags with class card
+//create div tags inside card with class card-img-container. See mockup, add employee image
+//create div tags inside card with class card-info-container.  See mockup, add h3 with first last, p with email and p with city state
+
+//CLICK EVENT LISTENER WHEN ANY PART OF EMPLOYEE ITEM IN THE DIRECTORY IS CLICKED A MODAL SHOULD POP UP WITH IMAGE, NAME, EMAIL, CITY OR LOCATION, CELL NUMBER (XXX) XXX-XXXX, DETAILED ADDRESS, BIRTHDAY MM/DD/YYYY, A WAY TO CLOSE THE MODAL WINDOW, REFER TO MOCKUPS AND COMMENTS
+
+//Hint to use this method: element.insertAdjacentHTML('beforeend', 'HTML string') Allows one to add strings of HTML to the DOM without disrupting what already exists in the DOM.
