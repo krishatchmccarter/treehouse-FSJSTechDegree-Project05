e185487b19ed3ea5fa5544b4cee86951afa0881d
Kris Hatch-McCarter <userkris169@yahoo.com>
2021-08-28 23:16:13 -0700

    fetchData helper function

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,51 @@
 //DOM SELECTION
 const gallery = document.getElementById("gallery");
 
-//SINGLE REQUEST TO GET AND DISPLAY 12 RANDOM USERS
+// ------------------------------------------
+//  FETCH FUNCTIONS
+//  SINGLE REQUEST TO GET AND DISPLAY 12 RANDOM USERS
+// ------------------------------------------
 
-fetch("https://randomuser.me/api/?results=12")
-  .then((response) => response.json())
-  .then((data) => console.log(data.results));
+function fetchData(url) {
+  return (
+    fetch(url)
+      //.then((res) => console.log(res))
+      //.then(checkStatus)
+      .then((res) => res.json())
+    //.catch((error) => console.log("Looks like there was a problem", error))
+  );
+}
 
-//INCLUDE IMAGE, FIRST NAME AND LAST NAME, EMAIL, CITY OR LOCATION (REFER TO MOCKUPS AND COMMENTS IN INDEX.HTML FOR WHAT SHOULD BE DISPLAYED AND HOW STYLED)
+// ------------------------------------------
+//  HELPER FUNCTIONS
+// ------------------------------------------
+
+fetchData("https://randomuser.me/api/?nat=us&results=12").then((data) => {
+  for (let i = 0; i < data.results.length; i++) {
+    const firstName = data.results[i].name.first;
+    const lastName = data.results[i].name.last;
+    const city = data.results[i].location.city;
+    const state = data.results[i].location.state;
+    const email = data.results[i].email;
+    const imgLarge = data.results[i].picture.large;
+    const imgMedium = data.results[i].picture.medium;
+    const imgThumbnail = data.results[i].picture.thumbnail;
+    const birthday = data.results[i].dob.date;
+    const cell = data.results[i].cell;
+
+    console.log(
+      `${firstName} ${lastName} ${city} ${state} ${email} ${imgLarge} ${imgMedium} ${imgThumbnail} ${birthday} ${cell}`
+    );
+  }
+});
 
 //create div tags with class card
 //create div tags inside card with class card-img-container. See mockup, add employee image
 //create div tags inside card with class card-info-container.  See mockup, add h3 with first last, p with email and p with city state
+
+// ------------------------------------------
+//  EVENT LISTENERS
+// ------------------------------------------
 
 //CLICK EVENT LISTENER WHEN ANY PART OF EMPLOYEE ITEM IN THE DIRECTORY IS CLICKED A MODAL SHOULD POP UP WITH IMAGE, NAME, EMAIL, CITY OR LOCATION, CELL NUMBER (XXX) XXX-XXXX, DETAILED ADDRESS, BIRTHDAY MM/DD/YYYY, A WAY TO CLOSE THE MODAL WINDOW, REFER TO MOCKUPS AND COMMENTS
 
