d88e1890c5b6e73c677fb39ec7e7893817d8e69d
Kris Hatch-McCarter <userkris169@yahoo.com>
2021-09-04 13:19:06 -0700

    createGalleryMarkup

--- js/scripts.js
+++ js/scripts.js
@@ -21,27 +21,24 @@ function fetchData(url) {
 // ------------------------------------------
 
 fetchData("https://randomuser.me/api/?nat=us&results=12").then((data) => {
-  for (let i = 0; i < data.results.length; i++) {
-    const firstName = data.results[i].name.first;
-    const lastName = data.results[i].name.last;
-    const city = data.results[i].location.city;
-    const state = data.results[i].location.state;
-    const email = data.results[i].email;
-    const imgLarge = data.results[i].picture.large;
-    const imgMedium = data.results[i].picture.medium;
-    const imgThumbnail = data.results[i].picture.thumbnail;
-    const birthday = data.results[i].dob.date;
-    const cell = data.results[i].cell;
-
-    console.log(
-      `${firstName} ${lastName} ${city} ${state} ${email} ${imgLarge} ${imgMedium} ${imgThumbnail} ${birthday} ${cell}`
-    );
-  }
+  createGalleryMarkup(data.results);
 });
 
-//create div tags with class card
-//create div tags inside card with class card-img-container. See mockup, add employee image
-//create div tags inside card with class card-info-container.  See mockup, add h3 with first last, p with email and p with city state
+function createGalleryMarkup(data) {
+  data.map((person) => {
+    person = ` <div class="card">
+<div class="card-img-container">
+    <img class="card-img" src="${person.picture.medium}" alt="profile picture">
+</div>
+<div class="card-info-container">
+    <h3 id="name" class="card-name cap">${person.name.first} ${person.name.last}</h3>
+    <p class="card-text">${person.email}</p>
+    <p class="card-text cap">${person.location.city} ${person.location.state}</p>
+</div>
+</div>`;
+    gallery.insertAdjacentHTML("beforeend", person);
+  });
+}
 
 // ------------------------------------------
 //  EVENT LISTENERS
