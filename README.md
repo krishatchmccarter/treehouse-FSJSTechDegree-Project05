de97b80f24b9779aba59d17ac6a211e97f50c160
Kris Hatch-McCarter <userkris169@yahoo.com>
2021-09-25 20:49:29 -0700

    FinishModalandComments

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-My Treehouse FSJS TechDegree Project 5 - Public API Requests
+My Treehouse FSJS TechDegree Project 5 - Public API Requests, by Kris Hatch-McCarter
 
 In this project, I built an employee directory for Awesome Startup, a fictional company. This project uses Javascript, Fetch API and a public API (https://randomuser.me/). Fetch is used to request results for 12 random "employees" and Javascript is used to dynamically update the HTML to display those results in a grid. When an image or name inthe grid is clicked, a modal window will open with more details about the employee.
--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,12 @@
 //DOM SELECTION
 const gallery = document.getElementById("gallery");
 
-// ------------------------------------------
-//  FETCH FUNCTIONS
-//  SINGLE REQUEST TO GET AND DISPLAY 12 RANDOM USERS
-// ------------------------------------------
-
+//Single request to get and display 12 random users
 function fetchData(url) {
-  return (
-    fetch(url)
-      //.then((res) => console.log(res))
-      //.then(checkStatus)
-      .then((res) => res.json())
-    //.catch((error) => console.log("Looks like there was a problem", error))
-  );
+	return fetch(url)
+		.then((res) => res.json())
+
+		.catch((error) => console.log("Looks like there was a problem", error));
 }
 
 // ------------------------------------------
@@ -21,12 +14,14 @@ function fetchData(url) {
 // ------------------------------------------
 
 fetchData("https://randomuser.me/api/?nat=us&results=12").then((data) => {
-  createGalleryMarkup(data.results);
+	personData = data.results;
+	createGalleryMarkup(personData);
+	createModal(personData);
 });
 
 function createGalleryMarkup(data) {
-  data.map((person) => {
-    person = ` <div class="card">
+	data.map((person) => {
+		person = ` <div class="card">
 <div class="card-img-container">
     <img class="card-img" src="${person.picture.medium}" alt="profile picture">
 </div>
@@ -36,14 +31,63 @@ function createGalleryMarkup(data) {
     <p class="card-text cap">${person.location.city} ${person.location.state}</p>
 </div>
 </div>`;
-    gallery.insertAdjacentHTML("beforeend", person);
-  });
+		gallery.insertAdjacentHTML("beforeend", person);
+	});
 }
 
-// ------------------------------------------
-//  EVENT LISTENERS
-// ------------------------------------------
+// Listens for click on person card to display modal
+function createModal(data) {
+	const cardDiv = document.getElementsByClassName("card");
+	for (let i = 0; i < data.length; i++) {
+		cardDiv[i].addEventListener("click", (e) => {
+			clickedPerson = data.indexOf(data[i]);
+			generateModalCard(data[clickedPerson]);
+		});
+	}
+}
 
-//CLICK EVENT LISTENER WHEN ANY PART OF EMPLOYEE ITEM IN THE DIRECTORY IS CLICKED A MODAL SHOULD POP UP WITH IMAGE, NAME, EMAIL, CITY OR LOCATION, CELL NUMBER (XXX) XXX-XXXX, DETAILED ADDRESS, BIRTHDAY MM/DD/YYYY, A WAY TO CLOSE THE MODAL WINDOW, REFER TO MOCKUPS AND COMMENTS
+// Creates markup for modal cards
+function generateModalCard(person) {
+	let modalCard = `<div class="modal-container">
+                          <div class="modal">
+                              <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                              <div class="modal-info-container">
+                                  <img class="modal-img" src="${
+																		person.picture.large
+																	}" alt="profile picture">
+                                  <h3 id="name" class="modal-name cap">${
+																		person.name.first
+																	} ${person.name.last}</h3>
+                                  <p class="modal-text">${person.email}</p>
+                                  <p class="modal-text cap">${
+																		person.location.city
+																	}</p>
+                                  <hr>
+                                  <p class="modal-text">${person.phone}</p>
+                                  <p class="modal-text">${
+																		person.location.street.number
+																	} ${person.location.street.name}, ${
+		person.location.city
+	}, ${person.location.state} ${person.location.postcode}</p>
+                                  <p class="modal-text">Birthday: ${person.dob.date.slice(
+																		5,
+																		7
+																	)}/${person.dob.date.slice(
+		8,
+		10
+	)}/${person.dob.date.slice(0, 4)}</p>
+                              </div>
+                          </div>
+                        `;
 
-//Hint to use this method: element.insertAdjacentHTML('beforeend', 'HTML string') Allows one to add strings of HTML to the DOM without disrupting what already exists in the DOM.
+	gallery.insertAdjacentHTML("beforeend", modalCard);
+
+	// Variables for modal
+	const modalContainer = document.querySelector(".modal-container");
+	const modalClose = document.querySelector(".modal-close-btn");
+
+	// Event listener on close button
+	modalClose.addEventListener("click", () => {
+		modalContainer.remove();
+	});
+}
